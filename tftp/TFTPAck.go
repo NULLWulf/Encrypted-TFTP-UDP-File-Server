ac9b3e2bea749d92eb2732299b24881ff24e10bb
Nathan Wolf <nwolf2@oswego.edu>
2023-03-02 02:53:17 -0500

    updated tftp ack, still need to do builder method

--- tftp/TFTPAck.go
+++ tftp/TFTPAck.go
@@ -1,7 +1,53 @@
 package tftp
 
+import (
+	"encoding/binary"
+	"errors"
+)
+
+// TFTPAcknowledgement TODO Add builder method
 // TFTPAcknowledgement represents a TFTP acknowledgement packet.
 type TFTPAcknowledgement struct {
 	Opcode      TFTPOpcode
 	BlockNumber uint16
+}
+
+//// Builder returns a builder for creating a TFTPAcknowledgement packet.
+//func AckBuilder() *TFTPAcknowledgementBuilder {
+//	return &TFTPAcknowledgementBuilder{}
+//}
+
+// ReadFromBytes reads a TFTPAcknowledgement packet from a byte slice.
+func (ack *TFTPAcknowledgement) ReadFromBytes(packet []byte) error {
+	// Check that the packet is at least 4 bytes long
+	if len(packet) < 4 {
+		return errors.New("packet too short")
+	}
+
+	// Check that the opcode is valid
+	opcode := binary.BigEndian.Uint16(packet[:2])
+	if opcode != uint16(TFTPOpcodeACK) {
+		return errors.New("invalid opcode")
+	}
+
+	// Parse the block number
+	blockNumber := binary.BigEndian.Uint16(packet[2:4])
+
+	// Set the fields in the acknowledgement packet
+	ack.Opcode = TFTPOpcodeACK
+	ack.BlockNumber = blockNumber
+
+	return nil
+}
+
+// ToBytes converts a TFTPAcknowledgement packet to a byte slice.
+func (ack *TFTPAcknowledgement) ToBytes() ([]byte, error) {
+	// Allocate a byte slice to hold the packet.
+	packet := make([]byte, 4)
+
+	// Set the opcode and block number in the packet.
+	binary.BigEndian.PutUint16(packet[:2], uint16(ack.Opcode))
+	binary.BigEndian.PutUint16(packet[2:4], ack.BlockNumber)
+
+	return packet, nil
 }
