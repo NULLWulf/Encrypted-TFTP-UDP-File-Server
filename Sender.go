ab5b4c03d20c81fcaad36db72e241c5bad2baefe
Nathan Wolf <nwolf2@oswego.edu>
2023-05-06 16:29:00 -0400

    this is working as it is

--- Sender.go
+++ Sender.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"log"
 	"math/big"
-	"math/rand"
 	"net"
 	"time"
 )
@@ -149,98 +148,5 @@ func (c *TFTPProtocol) sender(addr *net.UDPAddr) error {
 		return nil
 	}
 
-	return nil
-}
-
-/*
-sender2 is a modified version of sliding window that attempts to use concurrency to listen for ack packets while sending data packets.
-It is not currently used in the program, but it is left here for future reference.
-*/
-func (c *TFTPProtocol) sender2(addr *net.UDPAddr) error {
-	// ... (unchanged code)\/
-	// Initialize variables
-	var ack tftp.Ack //
-	log.Println("Starting sender transfer TFTP loop")
-	packet := make([]byte, 520)                             //Byte buffer
-	WindowSize, base, nextSeqNum, dropProb := 8, 1, 1, .20  //Window size, base, next sequence number, and drop probability
-	n, _ := c.conn.Read(tftp.Xor(packet, c.dhke.aes512Key)) //Read the initial ACK
-	c.ADti(n)                                               // Add to the total bytes received to running outgoing total
-	packet = packet[:n]                                     //Trim the packet to the size of the data received
-	err := ack.Parse(packet)                                //
-	// set timeout
-	c.conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-	log.Printf("Initial ACK received: %v\n", ack)
-	if err != nil {
-		return errors.New("error parsing ack packet: " + err.Error())
-	}
-	if ack.BlockNumber != 0 {
-		c.sendError(3, "Expected initial block number to be 0")
-		return errors.New("error parsing ack packet: block number should be 0, expecting initial block")
-	}
-
-	// Create channels for ACKs and errors
-	ackCh := make(chan tftp.Ack, WindowSize)
-	errCh := make(chan error)
-
-	// Goroutine to receive ACKs
-	go func() {
-		for {
-			n, _ := c.conn.Read(packet)
-			c.ADti(n)
-			packet = packet[:n]
-			opcode := binary.BigEndian.Uint16(packet[:2])
-			switch tftp.TFTPOpcode(opcode) {
-			case tftp.TFTPOpcodeACK:
-				err := ack.Parse(packet)
-				if err != nil {
-					errCh <- fmt.Errorf("error parsing ACK packet: %s", err)
-					return
-				}
-				ackCh <- ack
-			default:
-				errCh <- fmt.Errorf("received unexpected packet: %v", packet)
-				return
-			}
-		}
-	}()
-
-	// Send packets within the window size
-	for nextSeqNum < base+WindowSize && nextSeqNum <= len(c.dataBlocks) {
-		if rand.Float64() < dropProb && DropPax {
-			log.Printf("Dropped packet %d\n", nextSeqNum)
-			continue // do not increment nextSeqNum when a packet is dropped
-		}
-		packet = c.dataBlocks[nextSeqNum-1].ToBytes()
-		n, err = c.conn.WriteToUDP(packet, addr)
-		c.ADto(n)
-		log.Printf("Sending packet %d, %v", nextSeqNum, len(packet))
-		if err != nil {
-			c.sendAbort()
-			return fmt.Errorf("error sending data packet: %s", err)
-		}
-		nextSeqNum++
-	}
-
-	// Check for ACKs with a timeout
-	select {
-	case ack := <-ackCh:
-		log.Printf("Received ACK for packet %d\n", ack.BlockNumber)
-		if ack.BlockNumber >= uint16(base) {
-			base = int(ack.BlockNumber + 1)
-		}
-	case err := <-errCh:
-		log.Printf("Error: %s\n", err)
-		return err
-	case <-time.After(time.Duration(500) * time.Millisecond):
-		// Timeout: resend unacknowledged packets within the window
-		log.Printf("Timeout, resending unacknowledged packets\n")
-		nextSeqNum = base
-	}
-
-	// Check if all packets have been sent and acknowledged
-	if base > len(c.dataBlocks) {
-		log.Printf("All packets sent and acknowledged\n")
-		return nil
-	}
 	return nil
 }
